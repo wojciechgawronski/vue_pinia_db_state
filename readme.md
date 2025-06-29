43523c640e7e072776df4772773d1561095b61e0
W. Gawronski <wgawronski.wg@gmail.com>
2025-06-29 11:55:35 +0200

    init

---
+++ readme.md
@@ -0,0 +1,14 @@
+# pinia_tasks
+
+
+## Project Setup
+
+```sh
+npm install
+npm run dev
+npm run build
+npm run test:unit
+
+node -v
+nvm use 20
+```
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      hello world
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+  // import { RouterLink, RouterView } from 'vue-router'
+</script>
+
